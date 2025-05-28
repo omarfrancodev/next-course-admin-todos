20379a6830524a73a3a3f8211af29bc1d21cfc7e
omarfrancodev <fofe2803@gmail.com>
2025-05-28 16:50:29 -0600

    feat: adding POST and PUT endpoints for todo entity using yup to validate data and fields

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import { boolean, object, string } from 'yup';
 
 interface Segments {
     params: {
@@ -7,13 +8,33 @@ interface Segments {
     }
 }
 
+const getTodo = async (id: string) => await prisma.todo.findFirst({ where: { id } });
+
 
 export async function GET(request: Request, { params }: Segments) {
-    const { id } = await params;
+    const todo = await getTodo(params.id);
 
-    const todo = await prisma.todo.findFirst({ where: { id } });
-
-    if (!todo) return NextResponse.json({ message: `Todo with id ${id} not found` }, { status: 404 });
+    if (!todo) return NextResponse.json({ message: `Todo with id ${params.id} not found` }, { status: 404 });
 
     return NextResponse.json(todo, { status: 200 });
+}
+
+const putSchema = object({
+    description: string().optional(),
+    complete: boolean().optional(),
+});
+
+export async function PUT(req: Request, { params }: Segments) {
+    const todo = await getTodo(params.id);
+
+    if (!todo) return NextResponse.json({ message: `Todo with id ${params.id} not found` }, { status: 404 });
+    try {
+
+        const { description, complete } = await putSchema.validate(await req.json());
+        const updatedTodo = await prisma.todo.update({ where: { id: params.id }, data: { description, complete } });
+
+        return NextResponse.json(updatedTodo, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 });
+    }
 }
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import { boolean, object, string } from 'yup';
 
 interface CustomPaginationResponse {
     metadata: {
@@ -15,6 +16,22 @@ interface CustomPaginationResponse {
     data: object[]
 }
 
+const generateCustomPaginationResponse = (
+    data: object[], total: number, limit: number, offset: number, origin: string, pathname: string): CustomPaginationResponse => {
+    return {
+        metadata: {
+            total,
+            limit,
+            offset,
+        },
+        links: {
+            self: `${origin}${pathname}?limit=${limit}&offset=${offset}`,
+            prev: (offset - limit < 0) ? null : `${origin}${pathname}?limit=${limit}&offset=${offset - limit}`,
+            next: (offset + limit >= total) ? null : `${origin}${pathname}?limit=${limit}&offset=${offset + limit}`,
+        },
+        data
+    }
+}
 
 export async function GET(request: Request) {
     const { searchParams, origin, pathname } = new URL(request.url);
@@ -35,24 +52,25 @@ export async function GET(request: Request) {
     });
 
     const totalTodos = await prisma.todo.count();
-
-    const self = `${origin}${pathname}?limit=${limit}&offset=${offset}`;
-    const prev = (offset - limit < 0) ? null : `${origin}${pathname}?limit=${limit}&offset=${offset - limit}`;
-    const next = (offset + limit >= totalTodos) ? null : `${origin}${pathname}?limit=${limit}&offset=${offset + limit}`;
-
-    const response: CustomPaginationResponse = {
-        metadata: {
-            total: totalTodos,
-            limit: limit,
-            offset: offset,
-        },
-        links: {
-            self: self,
-            prev: prev,
-            next: next,
-        },
-        data: todos
-    }
+    const response: CustomPaginationResponse = generateCustomPaginationResponse(todos, totalTodos, limit, offset, origin, pathname);
 
     return NextResponse.json(response, { status: 200 });
+}
+
+const postSchema = object({
+    description: string().required(),
+    complete: boolean().optional().default(false),
+});
+
+export async function POST(req: Request) {
+    try {
+
+        const { description, complete } = await postSchema.validate(await req.json());
+
+        const todo = await prisma.todo.create({ data: { description, complete } });
+
+        return NextResponse.json(todo, { status: 201 });
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 });
+    }
 }
